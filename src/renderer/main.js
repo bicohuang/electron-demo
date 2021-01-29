1cb70c212ad185794aa6bf81b43c9aded9e94e86
bicohuang <894419115@qq.com>
2021-01-29 09:40:44 +0800

    20210129

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import axios from 'axios'
-import Antd from 'ant-design-vue';
 import App from './App'
 import router from './router'
 import store from './store'
-import 'ant-design-vue/dist/antd.css';
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
